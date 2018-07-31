3ea95d4a46d60c2565ff31ab68dab956b4343d79
Thomas Bartsch <36231480+thomasbartsch@users.noreply.github.com>
2018-07-31 12:30:20 +0200

    Update hero-detail.component.ts

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,7 +4,7 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: []
 })
 export class HeroDetailComponent implements OnInit {
 
