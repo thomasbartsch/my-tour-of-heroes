87b9e9be39f4e6003ed0c4aea5a05cdbb9cb1c3b
Thomas Bartsch <36231480+thomasbartsch@users.noreply.github.com>
2018-08-01 10:40:31 +0200

    Add files via upload

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'src/app/hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +12,23 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
-
-  constructor() { }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes =>
+      this.heroes = heroes
+    );
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
