59c2c27b0ef632da5f4e57888844e1a02ed3f219
Thomas Bartsch <36231480+thomasbartsch@users.noreply.github.com>
2018-08-16 14:44:39 +0200

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: []
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'My Tour of Heroes';
 }
